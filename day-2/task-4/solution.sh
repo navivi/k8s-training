c02d53b973df95c08025e447d57b1bf61687a313
navivi <34754379+navivi@users.noreply.github.com>
2025-03-31 09:52:27 +0300

    Update solution.sh

--- day-2/task-4/solution.sh
+++ day-2/task-4/solution.sh
@@ -72,7 +72,7 @@ spec:
     spec:
       containers:
       - name: lc-db # The container name
-        image: mongo # The DockerHub image
+        image: mongo:4.2 # The DockerHub image
         ports:
         - containerPort: 27017 # Open pod port 80 for the container
 EOF
