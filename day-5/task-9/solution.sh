660db8586ed0ce0b2d0fff878cb71d4c0755ae8a
navivi <34754379+navivi@users.noreply.github.com>
2025-04-03 10:19:50 +0300

    Update solution.sh

--- day-5/task-9/solution.sh
+++ day-5/task-9/solution.sh
@@ -26,7 +26,7 @@ spec:
     spec:
       containers:
       - name: lc-db # The container name
-        image: mongo # The DockerHub image
+        image: mongo:4.2 # The DockerHub image
         ports:
         - containerPort: 27017 # Open pod port 80 for the container
         livenessProbe:
@@ -64,6 +64,7 @@ spec:
       - name: data
         hostPath:
           path: /letschat/data
+          type: DirectoryOrCreate
       nodeSelector:
         app: letschat
 EOF
